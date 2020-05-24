81e1d853754ccbacba12a4e8ba8cb36656ba3bc5
kk289 <kevilkhadka@gmail.com>
2020-05-24 13:19:14 -0700

    file update

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
 # Stock_Price_Prediction
 
+Stock Name: AAPL (Apple)
+
 Stock Market Basic Influencing Factor:
 
 1. Closing Price
 2. Opening Price
 3. After-Hour Trading
+
+Coding
+
+1. Dataset Import
+
+$pandas datareader$ library allows us to connect to the website and extract data directly from internet sources in our case we are extracting data from Yahoo Finance API.
+
+Using following code:
+$df = web.DataReader("AAPL", 'yahoo', start, end)$
+
+Where:
+
+$start = datetime.datetime(2012, 1, 1)$
+
+$end = datetime.datetime(2020, 5, 22)$
+
+2. Checking Correlation
+
+Correlation is a measure of association or dependency between two features i.e. how much Y will vary with a variation in X. The correlation method that we will use is the Pearson Correlation.
+
+Using Pearson Correlation coefficient:
+$corr=df.corr(method='pearson')$
+
+Pearson Correlation Coefficient is the most popular way to measure correlation, the range of values varies from -1 to 1. In mathematics/physics terms it can be understood as if two features are positively correlated then they are directly proportional and if they share negative correlation then they are inversely proportional.
--- Stock_price_prediction.py
+++ Stock_price_prediction.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas_datareader.data as web
 import datetime
+import seaborn as sb
 # pandas_datareader library allows us to connect to the website and extract data directly from internet sources in our case we are extracting data from Yahoo Finance API.
 
 start = datetime.datetime(2012, 1, 1)
@@ -22,4 +23,61 @@ for x in range(len(df)):
     dates.append(newdate)
 df['dates'] = dates
 
+print(df.head(10))
 print(df.tail(10))
+
+df.reset_index(inplace=True)
+print(df.describe())
+
+df.describe(include=np.object)
+
+# Pearson Correlation Coefficient
+corr = df.corr(method='pearson')
+print(corr)
+
+# Visulize correlation
+corr_visual = sb.heatmap(corr,xticklabels=corr.columns, yticklabels=corr.columns,cmap='RdBu_r', annot=True, linewidth=0.5)
+plt.savefig("corr_visual.png")
+
+# Visualize the Dependent variable with Independent Features
+#prepare dataset to work with 
+appl_df = df[['Date','High','Open','Low','Close']]
+print(appl_df.head(20))
+plt.figure(figsize=(16,8))
+plt.title('Apple Stocks Closing Price History 2012-2020')
+plt.plot(appl_df['Date'],appl_df['Close'])
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Close Price US($)',fontsize=18)
+plt.style.use('fivethirtyeight')
+plt.show()
+
+# Plot Open vs Close (Year 2012)
+appl_df[['Open','Close']].head(50).plot(kind='bar',figsize=(16,8))
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
+
+# Plot Open vs Close (Year 2020)
+appl_df[['Open','Close']].tail(50).plot(kind='bar',figsize=(16,8))
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
+
+# Plot High vs Close (Year 2012)
+appl_df[['High','Close']].head(50).plot(kind='bar',figsize=(16,8))
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
+
+# Plot High vs Close (Year 2020)
+appl_df[['High','Close']].tail(50).plot(kind='bar',figsize=(16,8))
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
+
+
+
+
+
+
+
